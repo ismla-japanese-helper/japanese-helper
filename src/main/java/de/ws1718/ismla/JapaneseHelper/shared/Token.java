76403c6d81f456d8acf1e7f4f3224d7865cc178c
Verena Blaschke <verena.blaschke@gmx.de>
2018-01-24 16:02:40 +0100

    preprocessing class can deal with adjs + suru verbs

--- src/main/java/de/ws1718/ismla/JapaneseHelper/shared/Token.java
+++ src/main/java/de/ws1718/ismla/JapaneseHelper/shared/Token.java
@@ -15,4 +15,68 @@ public class Token {
 		this.translation = translation;
 	}
 
+	/**
+	 * @return the form
+	 */
+	public String getForm() {
+		return form;
+	}
+
+	/**
+	 * @param form
+	 *            the form to set
+	 */
+	public void setForm(String form) {
+		this.form = form;
+	}
+
+	/**
+	 * @return the pronunciation
+	 */
+	public String getPronunciation() {
+		return pronunciation;
+	}
+
+	/**
+	 * @param pronunciation
+	 *            the pronunciation to set
+	 */
+	public void setPronunciation(String pronunciation) {
+		this.pronunciation = pronunciation;
+	}
+
+	/**
+	 * @return the POS tag
+	 */
+	public String getPos() {
+		return pos;
+	}
+
+	/**
+	 * @param pos
+	 *            the POS tag to set
+	 */
+	public void setPos(String pos) {
+		this.pos = pos;
+	}
+
+	/**
+	 * @return the translation
+	 */
+	public String getTranslation() {
+		return translation;
+	}
+
+	/**
+	 * @param translation
+	 *            the translation to set
+	 */
+	public void setTranslation(String translation) {
+		this.translation = translation;
+	}
+
+	public void merge(Token other) {
+		// TODO
+	}
+
 }
