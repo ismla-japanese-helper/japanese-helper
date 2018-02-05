08d19c6347808eed4b92b815de74b089c8d08557
JI Xiang <jimmyjx51@gmail.com>
2018-02-05 15:41:08 +0100

    Added submission onClick handler

--- src/main/java/de/ws1718/ismla/JapaneseHelper/client/LookupServiceAsync.java
+++ src/main/java/de/ws1718/ismla/JapaneseHelper/client/LookupServiceAsync.java
@@ -1,11 +1,26 @@
 package de.ws1718.ismla.JapaneseHelper.client;
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import de.ws1718.ismla.JapaneseHelper.shared.Token;
 
 import java.util.List;
 
-// I'm not sure how to use the Async service yet. Let's leave it for later then.
 public interface LookupServiceAsync {
     void lookup(String sentence, AsyncCallback<List<Token>> callback);
+
+    public static final class Util {
+        private static LookupServiceAsync instance;
+
+        public static final LookupServiceAsync getInstance() {
+            if (instance == null) {
+                instance = (LookupServiceAsync) GWT.create(LookupService.class);
+            }
+            return instance;
+        }
+
+        private Util() {
+            // Utility class should not be instantiated
+        }
+    }
 }
